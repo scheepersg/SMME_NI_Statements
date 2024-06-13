d262f88d0826692fa5f03d1810570b4f16c8d6fb
Glenton Scheepers <50108276+scheepersg@users.noreply.github.com>
2024-06-13 14:29:37 +0200

    Update backend.tf

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ggs-dev-bucket"  # Replace with your actual S3 bucket name
+    bucket = "ggs-devops-testing"  # Replace with your actual S3 bucket name
     key    = "dev/terraform.tfstate"
     region = "af-south-1"  # Replace with your actual AWS region
   }
