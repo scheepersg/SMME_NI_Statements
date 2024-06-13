bbeda5139d9286a6e137fe614f3f110e785afdba
Glenton Scheepers <50108276+scheepersg@users.noreply.github.com>
2024-06-13 14:19:01 +0200

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,34 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "lambda_function_name" {
-  description = "Name of the Lambda function"
-  type        = string
-}
-
-variable "sns_topic_name" {
-  description = "Name of the SNS topic"
-  type        = string
-}
-
-variable "s3_bucket" {
-  description = "Name of the S3 bucket for Lambda deployment"
-  type        = string
-}
-
-variable "lambda_s3_key" {
-  description = "S3 key for the Lambda function zip file"
-  type        = string
-}
-
 # Create S3 bucket (if needed)
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.s3_bucket
+  acl = "private"
 }
 
 # IAM Role for Lambda Function
