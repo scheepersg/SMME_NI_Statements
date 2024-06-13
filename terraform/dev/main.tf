334e54ea5488aed3af234774f61e520c4ed4fbc2
Glenton Scheepers <50108276+scheepersg@users.noreply.github.com>
2024-06-13 17:07:09 +0200

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -79,7 +79,7 @@ resource "aws_lambda_function" "example_lambda" {
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_exec_role.arn
   s3_bucket     = var.dev_s3_bucket
-  s3_key        = var.lambda_s3_key
+  s3_key        = var.lambda_s3_key/lambda_function
 
   environment {
     variables = {
@@ -106,7 +106,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.example_lambda.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = var.lambda_s3_key
   }
 }
 
